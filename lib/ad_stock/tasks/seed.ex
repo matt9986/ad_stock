7fce8403856872ced42b026b67aeec27e5efdd0f
Skyler Parr <sparr@avvo.com>
2016-09-28 16:00:04 -0700

    added specialty seed

---
+++ lib/ad_stock/tasks/seed.ex
@@ -0,0 +1,22 @@
+defmodule AdStock.SpecialtySeed do
+
+  @specialty_ids [56,84,95,87,82,112,43,55,63,86]
+  @sales_region_ids [2302,3331,3330,3329,3333,3332]
+  
+  @specialties_base_url "http://solicitorstag.corp.avvo.com/api/2/specialties/"
+
+  alias AdStock.Specialty
+  alias AdStock.Repo
+
+  def run() do
+    Repo.delete_all(Specialty)
+    Enum.each(@specialty_ids, fn(specialty_id) -> 
+      specialty_data = Tesla.get("#{@specialties_base_url}#{specialty_id}.json").body
+      |> Poison.decode! 
+      |> Map.get("specialties") 
+      |> hd
+      specialty = %Specialty{specialty_id: Map.get(specialty_data, "id"), name: Map.get(specialty_data, "name")} |> Specialty.changeset
+      Repo.insert!(specialty)
+    end)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -37,6 +37,8 @@ defmodule AdStock.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:tesla, "~> 0.5.0"},
+     {:poison, ">= 1.0.0"},
      {:cowboy, "~> 1.0"}]
   end
 
