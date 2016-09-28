62f42743e54f8a6cdc7f5a4a50e8082cf10ebcdb
Matt <mpurdy@avvo.com>
2016-09-28 14:31:37 -0700

    Finish views for stocks and lawyers

--- web/views/api/lawyer_view.ex
+++ web/views/api/lawyer_view.ex
@@ -1,11 +1,21 @@
 defmodule AdStock.API.LawyerView do
   use AdStock.Web, :view
 
+  alias AdStock.API.LawyerView
+
   def render("index.json", %{lawyers: lawyers}) do
     %{lawyers: render_many(lawyers, LawyerView, "lawyer.json")}
   end
 
   def render("show.json", %{lawyer: lawyer}) do
     %{lawyers: render_many([lawyer], LawyerView, "lawyer.json")}
+  end
+
+  def render("lawyer.json", %{lawyer: lawyer}) do
+    %{
+      id: lawyer.id,
+      current_balance: lawyer.current_balance,
+      updated_at: lawyer.updated_at,
+    }
   end
 end
--- web/views/api/stock_view.ex
+++ web/views/api/stock_view.ex
@@ -1,7 +1,23 @@
 defmodule AdStock.API.StockView do
   use AdStock.Web, :view
 
+  alias AdStock.API.StockView
+
   def render("index.json", %{stocks: stocks}) do
     %{stocks: render_many(stocks, StockView, "stock.json")}
+  end
+
+  def render("stock.json", %{stock: stock}) do
+    %{
+      id: stock.id,
+      name: stock.name,
+      specialty_id: stock.specialty_id,
+      sales_region_id: stock.sales_region_id,
+      total_volume: stock.total_volume,
+      purchased_volume: stock.purchased_volume,
+      minimum_price: stock.minimum_price,
+      current_price: stock.current_price,
+      updated_at: stock.updated_at,
+    }
   end
 end
