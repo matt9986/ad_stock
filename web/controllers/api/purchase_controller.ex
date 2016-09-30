5a18b224889c6e6a73d2b84a0eacecdc1015c098
Skyler Parr <sparr@avvo.com>
2016-09-30 11:55:44 -0700

    added UI for purchasing stocks

--- web/controllers/api/purchase_controller.ex
+++ web/controllers/api/purchase_controller.ex
@@ -2,7 +2,7 @@ defmodule AdStock.API.PurchaseController do
   use AdStock.Web, :controller
 
   def create(conn, %{"stock_id" => stock_id, "quantity" => quantity, "lawyer_id" => lawyer_id}) do
-    response = AdStock.TransactionServer.create_purchase(stock_id, lawyer_id, quantity)
+    response = AdStock.TransactionServer.create_purchase("#{stock_id}" |> String.to_integer(), "#{lawyer_id}" |> String.to_integer(), "#{quantity}" |> String.to_integer())
     render(conn, "create.json", %{response: response})
   end
 end
