8e1964988638ac88dddf2212adad2d366a015c98
Matt <mpurdy@avvo.com>
2016-09-30 14:05:51 -0700

    Add lawyer login

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,8 @@ defmodule AdStock.PageController do
   use AdStock.Web, :controller
 
   def index(conn, _params) do
+    lawyers = AdStock.Repo.all(AdStock.Lawyer)
     stocks = AdStock.Repo.all(AdStock.Stock)
-    render conn, "index.html", stocks: stocks
+    render conn, "index.html", %{stocks: stocks, lawyers: lawyers}
   end
 end
