7cc434c28f02ebf7fb535c465bb3126da587ec16
Marc Schwieterman <mschwieterman@avvo.com>
2016-09-30 09:18:25 -0700

    Improve stub stock channel history data

--- web/channels/stock_channel.ex
+++ web/channels/stock_channel.ex
@@ -13,11 +13,31 @@ defmodule AdStock.StockChannel do
 
   def handle_info(:after_join, socket) do
     push socket, "history", %{"history": [
-      %{cratedAt: DateTime.to_iso8601(DateTime.utc_now), price: 1},
-      %{cratedAt: DateTime.to_iso8601(DateTime.utc_now), price: 2},
-      %{cratedAt: DateTime.to_iso8601(DateTime.utc_now), price: 3}
+      stub_transaction(hour: 0, price: 1),
+      stub_transaction(hour: 1, price: 2),
+      stub_transaction(hour: 2, price: 3),
+      stub_transaction(hour: 3, price: 4),
+      stub_transaction(hour: 4, price: 5),
+      stub_transaction(hour: 5, price: 4),
+      stub_transaction(hour: 6, price: 3),
+      stub_transaction(hour: 7, price: 4),
+      stub_transaction(hour: 8, price: 5),
+      stub_transaction(hour: 9, price: 6),
+      stub_transaction(hour: 10, price: 7),
+      stub_transaction(hour: 11, price: 8),
+      stub_transaction(hour: 12, price: 7),
+      stub_transaction(hour: 13, price: 6),
+      stub_transaction(hour: 14, price: 7),
+      stub_transaction(hour: 15, price: 8),
+      stub_transaction(hour: 16, price: 9)
     ]}
     {:noreply, socket}
+  end
+
+  defp stub_transaction([hour: hour, price: price]) do
+    {date, _} = :calendar.local_time()
+    date_time = Ecto.DateTime.from_erl({date, {hour, 1, 0}})
+    %{inserted_at: Ecto.DateTime.to_iso8601(date_time), price: price}
   end
 
   # Channels can be used in a request/response fashion
